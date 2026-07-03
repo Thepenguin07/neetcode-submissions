23c334c6c58d688e88cd25402a3d192e6e0206ac
Shifa Parveen <168899840+Thepenguin07@users.noreply.github.com>
2026-07-03 12:00:03 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+      int sum=(nums.length*(nums.length+1))/2,s=0;
+      for(int i=0;i<nums.length;i++){
+        s+=nums[i];
+      }
+      return sum-s;
+    }
+}
