84d69544719e857fc065433d405ef4b21d013bb7
Shifa Parveen <168899840+Thepenguin07@users.noreply.github.com>
2026-07-06 13:01:43 +0530

    Add: ransom-note - submission-2

---
+++ Data Structures & Algorithms/ransom-note/submission-2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int c1[]=new int[26];
+        for(char c:magazine.toCharArray())c1[c-'a']++;
+        for(char c:ransomNote.toCharArray()){
+            if(--c1[c-'a']<0)return false;
+        }
+        return true;
+    }
+}
