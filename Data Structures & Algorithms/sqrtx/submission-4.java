265c93099741155036f33fb3efaea7be25cf67b9
Shifa Parveen <168899840+Thepenguin07@users.noreply.github.com>
2026-08-08 21:33:49 +0530

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l=1,h=x,ans=0;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(m<=x/m){
+                ans=m;
+                l=m+1;
+            }
+         else h=m-1;
+        }return ans;
+    }
+}
