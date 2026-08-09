44469b2aba2fea0a9e9bf0e9c7568220c20b3f9c
Shifa Parveen <168899840+Thepenguin07@users.noreply.github.com>
2026-08-09 19:14:31 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> m=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+if(m.containsKey(nums[i])&&i-m.get(nums[i])<=k)return true;
+m.put(nums[i],i);
+        }
+        return false;
+    }
+}
